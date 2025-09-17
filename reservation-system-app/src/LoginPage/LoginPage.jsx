28f5097a4a6160c8ed50183c193c8afc0c4c917e
filipsupolik <filipsupolik25@gmail.com>
2025-09-17 12:15:50 +0200

    starting on Validation of Fields, Routing is dont for register and login page, submit button is not working

--- reservation-system-app/src/LoginPage/LoginPage.jsx
+++ reservation-system-app/src/LoginPage/LoginPage.jsx
@@ -4,22 +4,98 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
+import { Link as RouterLink } from "react-router-dom";
+import { useFormControl } from "@mui/material/FormControl";
+
 import "./LoginPage.css";
+import { useState } from "react";
 
 export default function LoginPage() {
   return (
     <div className="login-page-root">
       <Paper className="login-card" elevation={6}>
-        <Stack spacing={2} sx={{ width: '320px' }}>
-          <Typography variant="h5" component="h1" sx={{ textAlign: 'center' }}>Sign in</Typography>
+        <Stack spacing={2} sx={{ width: "320px" }}>
+          <Typography variant="h5" component="h1" sx={{ textAlign: "center" }}>
+            Sign in
+          </Typography>
           <TextField label="Email" variant="outlined" type="email" fullWidth />
-          <TextField label="Password" variant="outlined" type="password" fullWidth />
-          <Button variant="contained" color="primary">Sign in</Button>
-
-          <Typography variant="body2" sx={{ textAlign: 'center' }}>
-            Not a member?{' '}
-            <Link href="#" underline="hover">
+          <TextField
+            label="Password"
+            variant="outlined"
+            type="password"
+            fullWidth
+          />
+          <Button variant="contained" color="primary">
+            Sign in
+          </Button>
+          <Typography variant="body2" sx={{ textAlign: "center" }}>
+            Not a member?{" "}
+            <Link to="/register" component={RouterLink} underline="hover">
               Register
+            </Link>
+          </Typography>
+        </Stack>
+      </Paper>
+    </div>
+  );
+}
+
+export function RegisterPage() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [touched, setTouched] = useState(false);
+  return (
+    <div className="login-page-root">
+      <Paper className="login-card" elevation={6}>
+        <Stack spacing={2} sx={{ width: "320px" }}>
+          <Typography variant="h5" component="h1" sx={{ textAlign: "center" }}>
+            Sign up
+          </Typography>
+          <TextField
+            error={!firstName && touched}
+            label="First name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            variant="outlined"
+            type="text"
+            helperText={!firstName && touched ? "Field is required" : ""}
+            fullWidth
+            onBlur={(e) => setTouched(true)}
+          />
+          <TextField
+            error={!lastName && touched}
+            label="Last name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            variant="outlined"
+            type="text"
+            helperText={!lastName && touched ? "Field is required" : ""}
+            fullWidth
+            onBlur={(e) => setLastName(e.target.value)}
+          />
+          <TextField label="Email" variant="outlined" type="email" fullWidth />
+          <TextField
+            label="Password"
+            variant="outlined"
+            type="password"
+            fullWidth
+          />
+          <TextField
+            label="Confirm password"
+            variant="outlined"
+            type="password"
+            fullWidth
+          />
+          <Button variant="contained" color="primary">
+            Sign up
+          </Button>
+
+          <Typography variant="body2" sx={{ textAlign: "center" }}>
+            Alredy a member?{" "}
+            <Link to="/" component={RouterLink} underline="hover">
+              Sign in
             </Link>
           </Typography>
         </Stack>
--- reservation-system-app/src/main.jsx
+++ reservation-system-app/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage, { RegisterPage } from "./LoginPage/LoginPage";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
