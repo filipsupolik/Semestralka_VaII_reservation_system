40e547170e0aef99d3580ff1c2c608db2e6d9ea2
filipsupolik <filipsupolik25@gmail.com>
2025-10-14 17:40:45 +0200

    Setup backend project complete

---
+++ reservation-system-app-BE/src/main/java/com/skola/semestralka_vaii/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.skola.semestralka_vaii.config;
+
+public class SecurityConfig {
+}
---
+++ reservation-system-app-BE/src/test/java/com/skola/semestralka_vaii/SemestralkaVaiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skola.semestralka_vaii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemestralkaVaiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
